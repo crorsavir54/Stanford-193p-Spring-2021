25e5287ef447a88c5b83097710f287531a21dd61
Carlos Rivas <coriv@pm.me>
2021-07-24 16:16:35 +0800

    Initial Commit

---
+++ MemoryGame/MemoryGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryGameApp.swift
+//  MemoryGame
+//
+//  Created by coriv on 7/24/21.
+//
+
+import SwiftUI
+
+@main
+struct MemoryGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
